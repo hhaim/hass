68c599767298d0ae288d043eba39434ace33011e
Hanoch Haim <hhaim@cisco.com>
2019-10-14 20:01:14 +0300

    Add hebcal for holidays
    Signed-off-by: Hanoch Haim <hhaim@cisco.com>

--- apps/ada/test.py
+++ apps/ada/test.py
@@ -7,6 +7,14 @@ import schedule
 import temp_sensor
 import re
 import temp
+import requests 
+#from requests import get
+import urllib
+import json
+import pprint
+import pickle
+ 
+
 
 def test1 ():
   t1=datetime.time(17, 21,12)
@@ -188,19 +196,211 @@ def test7():
     t=datetime.datetime.now().strftime("%H:%M:%S")
     print(t)
 
-def main ():
-    test7 ()
-    
+def get_req():
+    #url = 'https://www.hebcal.com/hebcal/?i=on&b=28&m=50&v=1&cfg=json&maj=on&year=now&month=10&c=on&geo=pos&latitude=32.26&longitude=34.905&tzid=Asia/Jerusalem'
+    url = 'https://www.hebcal.com/hebcal/?i=on&b=28&m=50&v=1&cfg=json&maj=on&year=2020&c=on&geo=pos&latitude=32.26&longitude=34.905&tzid=Asia/Jerusalem'
+    r = requests.get(url)
+    s=r.text
+    return s
+
+HEBCAL_URL ='https://www.hebcal.com/hebcal/?i=on&b=28&m=50&v=1&cfg=json&maj=on&year={}&c=on&geo=pos&latitude={}&longitude={}&tzid={}'
+
+class TimeRec:
+        def __init__(self):
+            self.s = None # start time, date object 
+            self.e = None # end time, date object 
+            self.help = "" # string 
+            self.normal =False  # is it normal friday/sat
+        
+        def update(self):
+            if self.s.weekday() == 4 and self.e.weekday() ==5 and len(self.help)==0:
+                self.normal = True 
+
+        def __str__(self):
+            n=" "
+            if self.normal:
+                n="n"
+            s = "[{} : {}] {},{:20} {}".format(self.s.date(),self.e.date(),n,self.help,self.e-self.s)
+            return s
+
+def convToDateObject(s):
+    o = datetime.datetime.strptime(s, '%Y-%m-%dT%H:%M:%S%z')
+    return o
+
+
+class CalandarDb:
+
+        def __init__(self):
+            self.list = []
+            self.state = "off" 
+            self.latitude = 0
+            self.longitude = 0
+            self.tz = ""
+        
+        def set_cord(self,lat,long):
+            self.latitude = lat
+            self.longitude = long
+        
+        def set_tz(self,mtz):
+            self.tz = mtz
+
+        def load(self):
+            year = datetime.date.today().year
+            s = self._load_year(year)
+            s.extend(self._load_year(year+1))
+            self._process(s)
+
+        def _load_year(self,year):
+            url = HEBCAL_URL.format(year,self.latitude,self.longitude,self.tz)
+            r = requests.get(url)
+            y = json.loads(r.text)
+            return y['items']
+
+        def _update_list(self,now):
+            drop =0
+            for o in self.list:
+                if now > o.e:
+                    drop +=1
+                elif now < o.s:
+                    break;
+                else:
+                   self.state ="on"
+            if drop > 0:
+               self.list = self.list[drop:]  
+
+
+        def _process(self,items):
+            self.list =[]
+            state = "s"
+            for o in items:
+                c = o['category']
+                if state == "s":
+                    if c == "candles" : 
+                        d  = convToDateObject(o['date'])
+                        last = TimeRec()
+                        last.s =d 
+                        state = "e"
+                elif state == "e" :
+                    ischanukah = False  # there is a bug on chanuka 
+                    if c ==  "holiday":
+                        last.help = o['title']
+                        if "Chanukah" in o['title']:
+                            ischanukah = True 
+                    if c == "havdalah" or ischanukah : 
+                        d  = convToDateObject(o['date'])
+                        last.e = d 
+                        last.update()
+                        self.list.append(last)
+                        last = None
+                        state = "s"
+
+        def _dump(self):
+            cnt =0            
+            for o in self.list:
+                cnt+=1
+                print(" {:02d} : {} ".format(cnt,o))       
+        
+        def save_to_file(self,filename):
+            with open(filename, 'wb') as config_dictionary_file:
+               pickle.dump(self.list, config_dictionary_file)
+        
+        def load_from_file(self,filename):
+            with open(filename, 'rb') as config_dictionary_file:
+               self.list = pickle.load(config_dictionary_file)
 
 
 
-    
+def test3 ():
+    #c = CalandarDb()
+    #c.set_cord(32.26,34.905)
+    #c.set_tz("Asia/Jerusalem")
+    #c.load()
+    #c.save_to_file("c.data")
+    c1 = CalandarDb()
+    c1.load_from_file("c.data")
+    c1._dump()
+    #61 : [2021-02-05 : 2021-02-06] n,                     1 day, 1:19:00 
+    c1._update_list(convToDateObject("2021-02-06T18:07:00+02:00"))
+    print("========\n")
+    print(" state {} \n".format(c1.state))
+    c1._dump()
 
-for t in range(0,120):
-   f=t/10.0
-   print(temp.calc_heat_index_celsius(16.0+f, 70.0),16+f,)
 
 
 
 
-#main();
+
+def test2 ():
+    s = get_req()
+    l=[]
+    #s = '{"location":{"tzid":"Asia/Jerusalem","title":"32.26, 34.905, Asia/Jerusalem","geo":"pos","longitude":"34.905","latitude":"32.26"},"longitude":"34.905","date":"2019-10-08T09:44:37-00:00","latitude":"32.26","title":"Hebcal September 2019 32.26, 34.905, Asia/Jerusalem","items":[{"title":"Candle lighting: 6:30pm","hebrew":"הדלקת נרות","category":"candles","date":"2019-09-06T18:30:00+03:00"},{"date":"2019-09-07T19:47:00+03:00","category":"havdalah","hebrew":"הבדלה - 50 דקות","title":"Havdalah (50 min): 7:47pm"},{"category":"candles","date":"2019-09-13T18:21:00+03:00","hebrew":"הדלקת נרות","title":"Candle lighting: 6:21pm"},{"title":"Havdalah (50 min): 7:38pm","hebrew":"הבדלה - 50 דקות","category":"havdalah","date":"2019-09-14T19:38:00+03:00"},{"hebrew":"הדלקת נרות","title":"Candle lighting: 6:12pm","category":"candles","date":"2019-09-20T18:12:00+03:00"},{"title":"Havdalah (50 min): 7:28pm","hebrew":"הבדלה - 50 דקות","date":"2019-09-21T19:28:00+03:00","category":"havdalah"},{"date":"2019-09-27T18:02:00+03:00","category":"candles","hebrew":"הדלקת נרות","title":"Candle lighting: 6:02pm"},{"hebrew":"הבדלה - 50 דקות","title":"Havdalah (50 min): 7:19pm","category":"havdalah","date":"2019-09-28T19:19:00+03:00"},{"hebrew":"ערב ראש השנה","title":"Erev Rosh Hashana","link":"https://www.hebcal.com/holidays/rosh-hashana","date":"2019-09-29","category":"holiday","memo":"The Jewish New Year"},{"date":"2019-09-29T18:00:00+03:00","category":"candles","hebrew":"הדלקת נרות","title":"Candle lighting: 6:00pm"},{"category":"holiday","date":"2019-09-30","memo":"The Jewish New Year","hebrew":"ראש השנה 5780","title":"Rosh Hashana 5780","link":"https://www.hebcal.com/holidays/rosh-hashana","yomtov":true},{"title":"Candle lighting: 7:16pm","hebrew":"הדלקת נרות","date":"2019-09-30T19:16:00+03:00","category":"candles"}],"link":"https://www.hebcal.com/hebcal/?i=on;b=28;m=50;v=1;maj=on;year=2019;month=9;c=on;geo=pos;latitude=32.26;longitude=34.905;tzid=Asia%2FJerusalem"}'
+    #s= '{"title":"Hebcal October 2020 32.26, 34.905, Asia/Jerusalem","link":"https://www.hebcal.com/hebcal/?i=on&b=28&m=50&v=1&maj=on&year=2020&month=10&c=on&geo=pos&latitude=32.26&longitude=34.905&tzid=Asia%2FJerusalem","longitude":"34.905","items":[{"hebrew":"ערב סוכות","category":"holiday","memo":"Feast of Tabernacles","title":"Erev Sukkot","date":"2020-10-02","link":"https://www.hebcal.com/holidays/sukkot"},{"date":"2020-10-02T17:55:00+03:00","title":"Candle lighting: 5:55pm","category":"candles","hebrew":"הדלקת נרות"},{"link":"https://www.hebcal.com/holidays/sukkot","subcat":"major","title":"Sukkot I","hebrew":"סוכות יום א׳","category":"holiday","date":"2020-10-03","memo":"Feast of Tabernacles","yomtov":true},{"category":"havdalah","hebrew":"הבדלה - 50 דקות","title":"Havdalah (50 min): 7:12pm","date":"2020-10-03T19:12:00+03:00"},{"hebrew":"סוכות יום ב׳ (חול המועד)","category":"holiday","memo":"Feast of Tabernacles","title":"Sukkot II (CH''M)","date":"2020-10-04","link":"https://www.hebcal.com/holidays/sukkot","subcat":"major"},{"title":"Sukkot III (CH''M)","memo":"Feast of Tabernacles","category":"holiday","hebrew":"סוכות יום ג׳ (חול המועד)","link":"https://www.hebcal.com/holidays/sukkot","subcat":"major","date":"2020-10-05"},{"subcat":"major","link":"https://www.hebcal.com/holidays/sukkot","date":"2020-10-06","title":"Sukkot IV (CH''M)","memo":"Feast of Tabernacles","category":"holiday","hebrew":"סוכות יום ד׳ (חול המועד)"},{"date":"2020-10-07","subcat":"major","link":"https://www.hebcal.com/holidays/sukkot","category":"holiday","hebrew":"סוכות יום ה׳ (חול המועד)","title":"Sukkot V (CH''M)","memo":"Feast of Tabernacles"},{"date":"2020-10-08","subcat":"major","link":"https://www.hebcal.com/holidays/sukkot","category":"holiday","hebrew":"סוכות יום ו׳ (חול המועד)","title":"Sukkot VI (CH''M)","memo":"Feast of Tabernacles"},{"category":"holiday","hebrew":"סוכות יום ז׳ (הושענא רבה)","title":"Sukkot VII (Hoshana Raba)","memo":"Feast of Tabernacles","date":"2020-10-09","link":"https://www.hebcal.com/holidays/sukkot","subcat":"major"},{"date":"2020-10-09T17:46:00+03:00","hebrew":"הדלקת נרות","category":"candles","title":"Candle lighting: 5:46pm"},{"date":"2020-10-10","yomtov":true,"memo":"Eighth Day of Assembly","subcat":"major","link":"https://www.hebcal.com/holidays/shmini-atzeret","hebrew":"שמיני עצרת","category":"holiday","title":"Shmini Atzeret"},{"date":"2020-10-10T19:03:00+03:00","hebrew":"הבדלה - 50 דקות","category":"havdalah","title":"Havdalah (50 min): 7:03pm"},{"title":"Candle lighting: 5:38pm","hebrew":"הדלקת נרות","category":"candles","date":"2020-10-16T17:38:00+03:00"},{"date":"2020-10-17T18:55:00+03:00","hebrew":"הבדלה - 50 דקות","category":"havdalah","title":"Havdalah (50 min): 6:55pm"},{"date":"2020-10-23T17:30:00+03:00","category":"candles","hebrew":"הדלקת נרות","title":"Candle lighting: 5:30pm"},{"title":"Havdalah (50 min): 6:47pm","hebrew":"הבדלה - 50 דקות","category":"havdalah","date":"2020-10-24T18:47:00+03:00"},{"date":"2020-10-30T16:23:00+02:00","hebrew":"הדלקת נרות","category":"candles","title":"Candle lighting: 4:23pm"},{"date":"2020-10-31T17:40:00+02:00","category":"havdalah","hebrew":"הבדלה - 50 דקות","title":"Havdalah (50 min): 5:40pm"}],"date":"2020-07-01T05:43:00-00:00","latitude":"32.26","location":{"geo":"pos","latitude":"32.26","longitude":"34.905","tzid":"Asia/Jerusalem","title":"32.26, 34.905, Asia/Jerusalem"}}'
+    y = json.loads(s)
+    state = "s"
+    #pprint.pprint(y)
+    for o in y["items"]:
+        c = o['category']
+        if state == "s":
+           if c == "candles" : 
+               d  = convToDateObject(o['date'])
+               last = TimeRec()
+               last.s =d 
+               state = "e"
+        elif state == "e" :
+            ischanukah = False  # there is a bug on chanuka 
+            if c ==  "holiday":
+               last.help = o['title']
+               if "Chanukah" in o['title']:
+                   ischanukah = True 
+            if c == "havdalah" or ischanukah  : 
+               d  = convToDateObject(o['date'])
+               last.e = d 
+               last.update()
+               l.append(last)
+               last = None
+               state = "s"
+            if c == "candles": 
+               print(" == skip :",str(o))
+    cnt =0            
+    for o in l:
+       cnt+=1
+       print(" {:02d} : {} ".format(cnt,o))       
+
+                
+
+def is_date_betwean(s,e,m):
+    so =convToDateObject(s)
+    eo =convToDateObject(e)
+    mo =convToDateObject(m)
+    if mo>=so and mo<=eo:
+        return True
+    return False     
+
+def test4():
+    print(is_date_betwean("2020-05-20T20:31:00+03:00",
+    "2020-05-21T20:31:00+03:00","2020-05-21T20:32:00+03:00"))
+
+
+
+#for t in range(0,120):
+#   f=t/10.0
+#   print(temp.calc_heat_index_celsius(16.0+f, 70.0),16+f,)
+#main()
+def test():
+    s="2020-05-30T20:31:00+03:00"
+    o = datetime.datetime.strptime(s, '%Y-%m-%dT%H:%M:%S%z')
+    s1="2020-06-30T20:31:00+03:00"
+    o1 = datetime.datetime.strptime(s1, '%Y-%m-%dT%H:%M:%S%z')
+    d = o1 - o
+    if d > datetime.timedelta(days=1):
+        print(" hey")
+    print(d)
+
+    #print('Date:', o.date().year)
+    #print('Time:', o.time())
+    #print(o.weekday())
+
+test()
+#test3()
+#main()
+#print("sd")
---
+++ custom_components/hebcal/__init__.py
@@ -0,0 +1,393 @@
+"""
+
+Load jewish Holidays(Yom Kipur)/Candles/Havdalah from the web (www.hebcal.com) 
+The json information is converted to a table of records in this format:
+
+ * star_time: the date and time that start the holiday/sat
+ * stop_time: the date and time that end the holiday/sat
+ * normal: is it a normal sat?
+ * help: holiday name (if it is not normal)
+
+@start_time, there are two events triggered
+  -10 min
+  -0 min
+  with this information
+
+EVENT_TURN_PRE = "hebcal.pre_on"
+EVENT_TURN_ON = "hebcal.turn_on"
+
+
+
+@ end_time there are one events triggered 
+EVENT_TURN_OFF = "hebcal.turn_off"
+
+
+How it works:
+ It queries  www.hebcal.com with json information. It always queries 2 years (now.year and now.year+1)
+ To have enough information in the db (at least ~60 records, per year)
+ Once the active number of records is below a watermark(~10), there is another try to read. 
+
+KISS 
+
+info: there is a bug in www.hebcal.com website with Chanukah, so there is a simple hack to fix this 
+
+ hebcal:
+  debug: False/True # to enable debug messages 
+
+
+"""
+import logging
+import voluptuous as vol
+import copy
+import datetime
+import requests 
+import json
+import pickle
+from homeassistant.core import HomeAssistant, callback
+
+from homeassistant.components.binary_sensor import DEVICE_CLASSES_SCHEMA
+from homeassistant.const import (
+    ATTR_ENTITY_ID, ATTR_STATE, STATE_ON, STATE_OFF)
+from homeassistant.helpers import discovery
+from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers.entity_component import EntityComponent
+from homeassistant.helpers.entity import Entity
+from homeassistant.util import dt as dt_util
+from homeassistant.helpers import event, service
+from homeassistant.helpers.restore_state import RestoreEntity
+from homeassistant.helpers.event import async_track_utc_time_change
+from homeassistant.helpers.typing import HomeAssistantType, ConfigType
+from datetime import timedelta
+
+
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = "hebcal"
+
+ENTITY_ID = "hebcal.hebcal"
+DATA_KEY = 'hebcal.devices'
+
+DEFAULT_NAME = 'hebcal'
+CONF_DEBUG = "debug"
+
+
+# pylint: disable=no-value-for-parameter
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.Schema({
+           vol.Optional(CONF_DEBUG, default=False): cv.boolean,
+        }),
+    },
+    extra=vol.ALLOW_EXTRA,
+)
+
+DEPENDENCIES = ['discovery']
+
+async def async_setup(hass, config):
+    """Track the state of the sun."""
+    HebcalSensor(hass, config)
+    return True
+
+HEBCAL_URL = 'https://www.hebcal.com/hebcal/?i=on&b=28&m=50&v=1&cfg=json&maj=on&year={}&c=on&geo=pos&latitude={}&longitude={}&tzid={}'
+
+def convToDateObject(s):
+    o = datetime.datetime.strptime(s, '%Y-%m-%dT%H:%M:%S%z')
+    return o
+
+class TimeRec:
+        def __init__(self):
+            self.s = None  # start time, date object 
+            self.e = None  # end time, date object 
+            self.help = ""  # string 
+            self.normal = False  # is it normal friday/sat
+        
+        def update(self):
+            if self.s.weekday() == 4 and self.e.weekday() ==5 and len(self.help)==0:
+                self.normal = True 
+
+        def __str__(self):
+            n=" "
+            if self.normal:
+                n="n"
+            s = "[{} : {}] {},{:20} {}".format(self.s,self.e,n,self.help,self.e-self.s)
+            return s
+
+class CalandarDb:
+        
+        def __init__(self):
+            self.list = []
+            self.state = STATE_OFF
+            self.latitude = 0
+            self.longitude = 0
+            self.tz = ""
+            self.tindex = 0
+        
+        def set_cord(self, lat, long):
+            self.latitude = lat
+            self.longitude = long
+        
+        def set_tz(self, mtz):
+            self.tz = mtz
+
+        def loadExample(self):
+            self.list = []
+
+            t = TimeRec()
+            t.s = convToDateObject("2020-07-01T18:04:00+03:00")
+            t.e = convToDateObject("2020-07-01T18:05:00+03:00")
+            t.normal = True
+            t.help = ""
+            self.list.append(t)
+
+            t = TimeRec()
+            t.s = convToDateObject("2020-07-01T18:06:00+03:00")
+            t.e = convToDateObject("2020-07-01T18:07:00+03:00")
+            t.normal = True
+            t.help = ""
+            self.list.append(t)
+
+        def get_head(self):
+            return self.list[self.tindex]
+        
+        def inc_head(self):
+            self.tindex += 1
+        
+        def get_size(self):
+            return len(self.list) - self.tindex
+
+        def need_to_reread(self):
+
+            if self.get_size() < 10:
+                return True
+            return False     
+
+        def load(self):
+            year = datetime.date.today().year
+            s = self._load_year(year)
+            s.extend(self._load_year(year+1))
+            self._process(s)
+
+        def _load_year(self, year):
+            url = HEBCAL_URL.format(year, self.latitude, 
+                                    self.longitude, 
+                                    self.tz)
+            r = requests.get(url)
+            y = json.loads(r.text)
+            return y['items']
+
+        def _update_list(self, now):
+            drop = 0
+            for o in self.list:
+                if now > o.e:
+                    drop += 1
+                elif now < o.s:
+                    break
+                else:
+                    self.state = STATE_ON
+            if drop > 0:
+                self.list = self.list[drop:]
+
+        def _process(self, items):
+            self.list = []
+            state = "s"
+            for o in items:
+                c = o['category']
+                if state == "s":
+                    if c == "candles": 
+                        d = convToDateObject(o['date'])
+                        last = TimeRec()
+                        last.s = d 
+                        state = "e"
+                elif state == "e":
+                    ischanukah = False  # there is a bug on chanuka 
+                    if c == "holiday":
+                        last.help = o['title']
+                        if "Chanukah" in o['title']:
+                            ischanukah = True 
+                    if c == "havdalah" or ischanukah: 
+                        d = convToDateObject(o['date'])
+                        last.e = d 
+                        last.update()
+                        self.list.append(last)
+                        last = None
+                        state = "s"
+
+        def _dump(self):
+            cnt = 0            
+            for o in self.list:
+                cnt += 1
+                print(" {:02d} : {} ".format(cnt, o))       
+        
+        def save_to_file(self, filename):
+            with open(filename, 'wb') as config_dictionary_file:
+               pickle.dump(self.list, config_dictionary_file)
+        
+        def load_from_file(self, filename):
+            with open(filename, 'rb') as config_dictionary_file:
+               self.list = pickle.load(config_dictionary_file)
+
+
+STATE_ATTR_NEXT_S_EVENT = "start"
+STATE_ATTR_NEXT_E_EVENT = "end"
+STATE_ATTR_HELP_EVENT = "help"
+STATE_ATTR_NORM_EVENT = "normal"
+
+EVENT_TURN_PRE = "hebcal.pre_on"
+EVENT_TURN_ON = "hebcal.turn_on"
+EVENT_TURN_OFF = "hebcal.pre_off"
+
+
+class HebcalSensor(RestoreEntity):
+
+    entity_id = ENTITY_ID
+
+    """"""
+    def __init__(self, hass, conf):
+        """Initialize the sensor."""
+        self.hass = hass
+        hcfg = self.hass.config
+        self._lat = hcfg.latitude
+        self._lon = hcfg.longitude
+        self._tz = hcfg.time_zone
+        self._state = STATE_OFF
+        self._debug = conf.get(CONF_DEBUG)
+        self._cancel = None
+        self.start_loading_db()
+
+
+    def start_loading_db(self):
+        self._db = None 
+        self.do = None  
+        self._unsubscribe_auto_updater = async_track_utc_time_change(
+              self.hass, self._async_update_startup,
+                second=0)
+
+    async def async_added_to_hass(self):
+       # not used for now 
+       """Call when entity about to be added to Home Assistant."""
+       await super().async_added_to_hass()
+       state = await self.async_get_last_state()
+       if state is not None:
+           self._state = state.state
+           _LOGGER.info(" Hebcal async_added_to_hass %s",str(state.attributes)) 
+
+
+    async def _async_update_startup(self, now):
+        """Load the db from the web"""
+        # have the info 
+        try:
+          await self.hass.async_add_executor_job(self.load_db_from_web)
+        except Exception as err:
+            _LOGGER.error("Error while trying to get hebcal: %s", err)
+        if self._db != None:  # we manage to get the db, else retry 
+            self.load_timers()
+            if self._unsubscribe_auto_updater != None:
+                self._unsubscribe_auto_updater()
+        self.async_schedule_update_ha_state()     
+
+    def load_db_from_web(self):    
+        db = CalandarDb()
+        db.set_cord(self._lat,self._lon)
+        db.set_tz(self._tz)
+        if self._debug:
+           _LOGGER.error(" {} {} {} ".format(self._lat,self._lon,self._tz)) 
+        db.load()
+        db._update_list(dt_util.now())
+        db._dump()
+        self._db = db
+        if self._debug:
+            _LOGGER.error(" db loaded {}".format(str(db))) 
+    
+    def _get_d(self, full):
+        d = {}
+        if full:
+          d = self.state_attributes()
+        d[ATTR_ENTITY_ID] = self.entity_id
+        return d
+
+    @callback
+    def _pre_on(self, now):
+        if self._debug:
+            _LOGGER.error(" ==> pre_on  ")
+        self.hass.bus.async_fire(EVENT_TURN_PRE, self._get_d(True)) 
+        self.async_schedule_update_ha_state()     
+
+    @callback
+    def _on(self, now):
+        self._state = STATE_ON
+        if self._debug:
+            _LOGGER.error(" ==>  on  ")
+        self.hass.bus.async_fire(EVENT_TURN_ON, self._get_d(True)) 
+        self.async_schedule_update_ha_state()     
+
+    @callback
+    def _off(self, now):
+        self._state = STATE_OFF
+        if self._debug:
+           _LOGGER.error(" ==>  off  ")
+        self.hass.bus.async_fire(EVENT_TURN_OFF, self._get_d(False)) 
+        self._db.inc_head()
+        if self._db.need_to_reread():
+            self.start_loading_db()
+        else:
+            self.load_timers()
+        self.async_schedule_update_ha_state()     
+
+
+    # load the timers from the database 
+    def load_timers(self):
+        if self._db.state == STATE_OFF:
+            do = self._db.get_head()
+            self.do = do
+            if self._debug:
+               _LOGGER.error(" start start {} ".format(self.do)) 
+            #trigger 3 events 
+            if do.e - do.s < datetime.timedelta(days=1):
+                 _LOGGER.error(" hebcal something wrong with the record {} ".format(self.do)) 
+
+            event.async_track_point_in_time(
+                self.hass, self._pre_on, do.s - timedelta(minutes=10))
+            event.async_track_point_in_time(
+                self.hass, self._on, do.s)
+            event.async_track_point_in_time(
+                self.hass, self._off, do.e)
+    
+        else:
+            self._db.state = STATE_OFF
+            do = self._db.get_head() 
+            event.async_track_point_in_time(
+                self.hass, self._off_on, dt_util.now())
+            event.async_track_point_in_time(
+                self.hass, self._off_off, do.e)
+
+
+    @property
+    def name(self):
+        """Return the name."""
+        return "Hebcal"
+
+    @property
+    def state(self):
+        """Return the state of the sun."""
+        return self._state
+
+    @property
+    def should_poll(self):
+       """No polling needed."""
+       return False
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit this state is expressed in."""
+        return "sec"
+
+    @property
+    def state_attributes(self):
+        """Return the state attributes."""
+        if self.do != None:
+            return { STATE_ATTR_NEXT_S_EVENT  : self.do.s,
+                     STATE_ATTR_NEXT_E_EVENT  : self.do.e,
+                     STATE_ATTR_HELP_EVENT    : self.do.help, 
+                     STATE_ATTR_NORM_EVENT    : self.do.normal } 
+        else:
+            return { }
