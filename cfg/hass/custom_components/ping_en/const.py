f2d8e2e1318bfd89ed3adf04b4adc0c919fbcea0
Hanoh Haim <hhaim@cisco.com>
2023-07-21 19:10:33 +0300

    ping: add more attr (loss, jitter,alive) to the ping sensor
    Signed-off-by: Hanoh Haim <hhaim@cisco.com>

---
+++ cfg/hass/custom_components/ping_en/const.py
@@ -0,0 +1,20 @@
+"""Tracks devices by sending a ICMP echo request (ping)."""
+
+from homeassistant.const import Platform
+
+# The ping binary and icmplib timeouts are not the same
+# timeout. ping is an overall timeout, icmplib is the
+# time since the data was sent.
+
+# ping binary
+PING_TIMEOUT = 3
+
+# icmplib timeout
+ICMP_TIMEOUT = 1
+
+PING_ATTEMPTS_COUNT = 3
+
+DOMAIN = "ping_en"
+PLATFORMS = [Platform.BINARY_SENSOR]
+
+PING_PRIVS = "ping_privs"
